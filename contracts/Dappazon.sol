58f17359647ce27d2cbae1547b8d64946423e7dd
Shubham <shubhamextraxyz@gmail.com>
2023-09-21 20:28:57 +0530

    Initial-commit

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+
+// STEP -1 -------------------------------------------------------------------------
+contract Dappazon {
+
+// STEP -2 ------------------------------------------------------------------------
+        address public owner;
+
+        constructor(){
+            owner = msg.sender; // msg.sender address inside constructer is of deployer, and 1st address of ethers.getSigners() is always deployer
+            // bydefault. 
+        }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,25 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+// STEP -1 ---------------------------
 describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
 
+// STEP -3 ---------------------------
+    beforeEach(async () => {
+      // Setup accounts
+      [deployer, buyer] = await ethers.getSigners() // we don't need to run hardhat node(local blockchain) to execute this specific code.
+
+      // Deploy contract
+      const Dappazon = await ethers.getContractFactory("Dappazon")
+      dappazon = await Dappazon.deploy()
+    })
+
+// STEP -4 ---------------------------
+    describe("Deployment", () => {
+      it("Sets the owner", async () => {
+        expect(await dappazon.owner()).to.equal(deployer.address) // 1st address of ethers.getSigners() is deployer of contract by default
+      })
+    })
 })
